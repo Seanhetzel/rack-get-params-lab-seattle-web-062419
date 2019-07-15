5aed8b63f98766c536166659539bb3a81ecd8098
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 01:22:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,7 +30,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       #else
-        resp.write "We don't have that item"
+        #resp.write "We don't have that item"
       #end
 
     else
