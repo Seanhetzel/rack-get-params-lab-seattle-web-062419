46e133a7cb1cec1c7ecadfe32ee63953809c5f9b
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 00:52:48 +0000

    Automatically backed up by Learn

---
+++ app/cart.rb
@@ -0,0 +1,14 @@
+class Cart
+
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack.Request.new(env)
+
+    if req.path.match(/cart/)
+      @@cart.each do |product|
+        resp.write "#{product}\n"
+         
+
+end
