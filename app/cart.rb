9644f9d2be0f486b1147007dcb86fdaba91fd630
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 01:00:29 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -9,6 +9,14 @@ class Cart
     if req.path.match(/cart/)
       @@cart.each do |product|
         resp.write "#{product}\n"
+      end
+    elsif req.path.match(/add/)
+      adding_product = req.params["q"]
+      if @@items.include?(adding_product)
+        @@cart << adding_product
+      else
+        resp.write "Coulnd't find #{adding_product}"
+
 
 
 end
