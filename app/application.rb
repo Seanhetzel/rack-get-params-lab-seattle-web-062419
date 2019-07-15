86308fe3dbe811d12cbe162d3b57829963f2d0ea
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 01:24:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,12 +26,12 @@ class Application
       end
 
     elsif req.path.match(/add/)
-      #if @@items.include?(search_term)
+      if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
-      #else
-        #resp.write "We don't have that item"
-      #end
+      else
+        resp.write "We don't have that item"
+      end
 
     else
       resp.write "Path Not Found"
