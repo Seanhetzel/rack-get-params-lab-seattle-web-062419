3c7beb8e4e8361eb0476cea5cb0bb730d518049e
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-15 01:24:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
 
     elsif req.path.match(/add/)
       if @@items.include?(search_term)
-        @@cart << search_term
+        @@cart.push(search_term)
         resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
